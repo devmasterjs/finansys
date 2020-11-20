d9eca5aa3ebf307a16751e76b213548dde88f4fb
Marlo Eichenberg Motta <devmasterjs@programmerhero.com.br>
2020-11-19 22:41:07 -0300

    feat(category): add component catetory-form

--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoryListComponent } from './category-list/category-list.component';
+import { CategoryFormComponent } from './category-form/category-form.component';
 
 
 @NgModule({
-  declarations: [CategoryListComponent],
+  declarations: [CategoryListComponent, CategoryFormComponent],
   imports: [
     CommonModule,
     CategoriesRoutingModule
